3cc945cecbabe2aac511773e88ab6a2e29606fcf
Vega <vegafernando881@gmail.com>
2023-12-11 13:36:22 -0600

    Cambiar la Alerta en el Formato Payment

--- controller/js/form_date_filter.js
+++ controller/js/form_date_filter.js
@@ -3,3 +3,17 @@ const inputDate1 = document.getElementById('date1');
 const inputDate2 = document.getElementById('date2');
 
 const validDate = '';
+
+// Alerta Personalizada 
+const { value: date } = await Swal.fire({
+    title: "select departure date",
+    input: "date",
+    didOpen: () => {
+      const today = (new Date()).toISOString();
+      Swal.getInput().min = today.split("T")[0];
+    }
+  });
+  if (date) {
+    Swal.fire("Departure date", date);
+  }
+  
